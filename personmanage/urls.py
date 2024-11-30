e407758a52fe2ee7ae969501e073812563b3dc52
sweeps-011 <wjj1648166133@163.com>
2024-11-30 11:02:07 +0800

    版本更新

---
+++ personmanage/urls.py
@@ -0,0 +1,55 @@
+"""
+URL configuration for personmanage project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app01 import views
+from app01.views import user,depart,admin,account
+
+
+
+urlpatterns = [
+    #    path('admin/', admin.site.urls),
+    #      部门管理
+         path('depart/list',depart.depart_list),
+         path('depart/add',depart.depart_add),
+
+         path('depart/modelform/add',depart.depart_add_modelform),
+
+         path('depart/delete',depart.depart_delete),
+         path('depart/<int:nid>/change',depart.depart_change),
+
+
+
+        # 用户管理
+         path('user/list',user.user_list),
+         path('user/add',user.user_add),
+         path('user/modelform/add',user.user_modelformadd),
+         path('user/delete',user.user_delete),
+         path('user/<int:id>/change',user.user_change),
+
+        #管理员
+         path('admin/list',admin.admin_list),
+         path('admin/add',admin.admin_add),
+         path('admin/delete',admin.admin_delete),
+         path('admin/<int:id>/change',admin.admin_change),
+         path('admin/change/<int:id>/password',admin.admin_change_password),
+
+         #登录
+         path('login/',account.account_login),
+         path('login/out',account.account_out)
+
+]
